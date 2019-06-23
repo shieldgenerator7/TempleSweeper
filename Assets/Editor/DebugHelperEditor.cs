ee5518f5e88b147746c5d2da572cd1f17b9ac702
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-06-22 22:15:01 -0400

    Added Debug Helper

---
+++ Assets/Editor/DebugHelperEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DebugHelper))]
+public class DebugHelperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        DebugHelper dh = ((DebugHelper)target);
+        if (GUILayout.Button("Highlight Tiles"))
+        {
+            dh.highlightAll();
+        }
+        if (GUILayout.Button("Reveal Tiles"))
+        {
+            dh.revealAll();
+        }
+    }
+}
---
+++ Assets/Scripts/DebugHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugHelper : MonoBehaviour
+{
+    public LevelTile.TileType selectType;
+    public Color highlightColor = Color.blue;
+
+    /// <summary>
+    /// Highlights all the tiles of the selected type
+    /// </summary>
+    public void highlightAll()
+    {
+        foreach (LevelTile lt in FindObjectsOfType<LevelTile>())
+        {
+            if (lt.tileType == selectType)
+            {
+                if (lt.cover)
+                {
+                    lt.cover.GetComponent<SpriteRenderer>().color = highlightColor;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Reveals all the tiles of the selected type
+    /// </summary>
+    public void revealAll()
+    {
+        foreach (LevelTile lt in FindObjectsOfType<LevelTile>())
+        {
+            if (lt.tileType == selectType)
+            {
+                lt.Revealed = true;
+            }
+        }
+    }
+}
