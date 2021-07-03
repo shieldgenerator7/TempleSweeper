9095f0aa49ccabde6b25d8823e3188bb7e873f78
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-07-03 19:20:23 -0400

    Import improved CustomMenu

--- Assets/Editor/CustomMenu.cs
+++ Assets/Editor/CustomMenu.cs
@@ -1,21 +1,114 @@
 ﻿using UnityEngine;
 using System.Diagnostics;
 using UnityEditor;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+using UnityEditor.SceneManagement;
+using System;
+using System.Reflection;
 
 public class CustomMenu
-{//2019-04-20: copied from Stonicorn.CustomMenu
+{//2021-07-03: copied from StonicornWorlds.CustomMenu
+
+    #region Editor Menu
+
+
+    //Find Missing Scripts
+    //2018-04-13: copied from http://wiki.unity3d.com/index.php?title=FindMissingScripts
+    static int go_count = 0, components_count = 0, missing_count = 0;
+    [MenuItem("SG7/Editor/Refactor/Find Missing Scripts")]
+    private static void FindMissingScripts()
+    {
+        go_count = 0;
+        components_count = 0;
+        missing_count = 0;
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene s = SceneManager.GetSceneAt(i);
+            if (s.isLoaded)
+            {
+                foreach (GameObject go in s.GetRootGameObjects())
+                {
+                    FindInGO(go);
+                }
+            }
+        }
+        Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing", go_count, components_count, missing_count));
+    }
+    private static void FindInGO(GameObject g)
+    {
+        go_count++;
+        Component[] components = g.GetComponents<Component>();
+        for (int i = 0; i < components.Length; i++)
+        {
+            components_count++;
+            if (components[i] == null)
+            {
+                missing_count++;
+                string s = g.name;
+                Transform t = g.transform;
+                while (t.parent != null)
+                {
+                    s = t.parent.name + "/" + s;
+                    t = t.parent;
+                }
+                Debug.Log(s + " has an empty script attached in position: " + i, g);
+            }
+        }
+        // Now recurse through each child GO (if there are any):
+        foreach (Transform childT in g.transform)
+        {
+            FindInGO(childT.gameObject);
+        }
+    }
+    #endregion
+
+    #region Prebuild Submenu
+    [MenuItem("SG7/Build/Prebuild/Run All Prebuild Tasks &w")]
+    public static int runAllPrebuildTasks()
+    {
+        ClearLogConsole();
+        Debug.Log("=== Prebuild tasks starting ===");
+        int problemCount = 0;
+        //
+        //problemCount += setupInputManager();
+        //
+        if (problemCount == 0)
+        {
+            Debug.Log("=== Prebuild tasks finished ===");
+        }
+        else
+        {
+            Debug.Log("=== Prebuild tasks: " + problemCount + " problems.");
+        }
+        return problemCount;
+    }
+
+    //2021-05-25: copied from http://answers.unity.com/answers/1318322/view.html
+    public static void ClearLogConsole()
+    {
+        Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+        Type logEntries = assembly.GetType("UnityEditor.LogEntries");
+        MethodInfo clearConsoleMethod = logEntries.GetMethod("Clear");
+        clearConsoleMethod.Invoke(new object(), null);
+    }
+    #endregion
+
+    #region Build / Run Menu
 
     [MenuItem("SG7/Build/Build Windows %w")]
     public static void buildWindows()
     {
         build(BuildTarget.StandaloneWindows, "exe");
     }
-    [MenuItem("SG7/Build/Build Linux %l")]
+    [MenuItem("SG7/Build/Build Linux")]
     public static void buildLinux()
     {
-        build(BuildTarget.StandaloneLinux, "x86");
+        Debug.LogError(
+            "Building Linux has not been readded yet after Unity removed it in 2019.2"
+            );
     }
-    [MenuItem("SG7/Build/Build Mac OS X %#l")]
+    [MenuItem("SG7/Build/Build Mac OS X")]
     public static void buildMacOSX()
     {
         build(BuildTarget.StandaloneOSX, "");
@@ -33,13 +126,11 @@ public class CustomMenu
 
         // User hit the cancel button.
         if (buildName == "")
-        {
             return;
-        }
 
         string path = buildName.Substring(0, buildName.LastIndexOf("/"));
-        UnityEngine.Debug.Log("BUILDNAME: " + buildName);
-        UnityEngine.Debug.Log("PATH: " + path);
+        Debug.Log("BUILDNAME: " + buildName);
+        Debug.Log("PATH: " + path);
 
         string[] levels = new string[EditorBuildSettings.scenes.Length];
         for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
@@ -68,7 +159,7 @@ public class CustomMenu
     {//2018-08-10: copied from build()
         string extension = "exe";
         string buildName = getBuildNamePath(extension);
-        UnityEngine.Debug.Log("Launching: " + buildName);
+        Debug.Log("Launching: " + buildName);
         // Run the game (Process class from System.Diagnostics).
         Process proc = new Process();
         proc.StartInfo.FileName = buildName;
@@ -84,9 +175,25 @@ public class CustomMenu
         EditorUtility.RevealInFinder(buildName);
     }
 
+    [MenuItem("SG7/Run/Open App Data Folder &f")]
+    public static void openAppDataFolder()
+    {
+        string filePath = Application.persistentDataPath + "/merky.txt";
+        if (System.IO.File.Exists(filePath))
+        {
+            EditorUtility.RevealInFinder(filePath);
+        }
+        else
+        {
+            EditorUtility.RevealInFinder(Application.persistentDataPath);
+        }
+    }
+
     public static string getDefaultBuildPath()
     {
-        return "Builds/" + PlayerSettings.productName + "_" + PlayerSettings.bundleVersion.Replace(".", "_");
+        return System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments)
+            + "/Unity/" + PlayerSettings.productName.Replace(" ", "") + "/Builds/"
+            + PlayerSettings.productName + "_" + PlayerSettings.bundleVersion.Replace(".", "_");
     }
     public static string getBuildNamePath(string extension, bool checkFolderExists = true)
     {
@@ -98,4 +205,48 @@ public class CustomMenu
         string buildName = defaultPath + "/" + PlayerSettings.productName + "." + extension;
         return buildName;
     }
+    #endregion
+
+    #region Session / Upgrade Menu
+
+    [MenuItem("SG7/Session/Begin Session")]
+    public static void beginSession()
+    {
+        Debug.Log("=== Beginning session ===");
+        string oldVersion = PlayerSettings.bundleVersion;
+        string[] split = oldVersion.Split('.');
+        string newNum = "" + (int.Parse(split[1]) + 1);
+        while (newNum.Length < 3)
+        {
+            newNum = "0" + newNum;
+        }
+        string newVersion = split[0] + "." + newNum;
+        PlayerSettings.bundleVersion = newVersion;
+        //Save and Log
+        EditorSceneManager.SaveOpenScenes();
+        Debug.LogWarning("Updated build version number from " + oldVersion + " to " + newVersion);
+        //Open folders
+        openAppDataFolder();
+    }
+
+    [MenuItem("SG7/Session/Finish Session")]
+    public static void finishSession()
+    {
+        Debug.Log("=== Finishing session ===");
+        int problemCount = runAllPrebuildTasks();
+        if (problemCount == 0)
+        {
+            EditorSceneManager.SaveOpenScenes();
+            buildWindows();
+        }
+        //Open folders
+        openBuildFolder();
+    }
+
+    [MenuItem("SG7/Upgrade/Force save all assets")]
+    public static void forceSaveAllAssets()
+    {
+        AssetDatabase.ForceReserializeAssets();
+    }
+    #endregion
 }
